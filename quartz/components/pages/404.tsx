99bff9131d2bdd0c35089cbf0b0e94ab931675a5
Gaggioaxel <gaggioaxel@yahoo.it>
2024-02-24 00:21:14 +0100

    Quartz sync: Feb 24, 2024, 12:21 AM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,12 +1,14 @@
 import { i18n } from "../../i18n"
+import { pathToRoot } from "../../util/path"
 import { QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
-function NotFound({ cfg }: QuartzComponentProps) {
+function NotFound({ fileData, cfg }: QuartzComponentProps) {
+  const baseDir = pathToRoot(fileData.slug!)
   return (
     <article class="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href="/" style={{ fontSize: "larger" }}>{i18n(cfg.locale).pages.error.getBackHome}</a>
+      <a href={baseDir} style={{ fontSize: "larger" }}>{i18n(cfg.locale).pages.error.getBackHome}</a>
     </article>
   )
 }
